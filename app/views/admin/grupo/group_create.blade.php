cd78872917d02e3eb4f18b283dea11ec68fffb1c
hectorz11 <hector.rz11@gmail.com>
2015-07-17 17:21:56 -0500

    Crear grupo en Sentry

--- app/views/admin/grupo/group_create.blade.php
+++ app/views/admin/grupo/group_create.blade.php
@@ -21,163 +21,164 @@
                 <div class="col-md-2 col-xs-3 col-sm-2"><label class="control-label"> Permisos:</label></div>
             </div>
             <div class="row">
-                <div class="col-md-2 col-xs-3 col-sm-2">
+                <div class="col-md-3 col-xs-4 col-sm-3">
+                    <div class="col-md-2">{{ Form::checkBox('admin', 1) }}</div>
+                    <div class="col-md-10">Administrador</div>
+                </div>
+            </div><hr>
+            <div class="row">
+                <div class="col-md-3 col-xs-4 col-sm-3">
                     <div class="row">
-                        <div class="col-md-2">{{ Form::checkBox('admin', Input::old('admin', 1)) }}</div>
-                        <div class="col-md-10">Administrador</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-2">{{ Form::checkBox('grupo', Input::old('grupo', 1)) }}</div>
+                        <div class="col-md-2">{{ Form::checkBox('grupo', 1) }}</div>
                         <div class="col-md-10">Grupos</div>  
                     </div>
                     <div class="row">
-                        <div class="col-md-2">{{ Form::checkBox('grupo.create', Input::old('grupo.create', 1)) }}</div>
+                        <div class="col-md-2">{{ Form::checkBox('grupo_create', 1) }}</div>
                         <div class="col-md-10">Grupo - Crear</div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2">{{ Form::checkBox('grupo.update', Input::old('grupo.update', 1)) }}</div>
+                        <div class="col-md-2">{{ Form::checkBox('grupo_update', 1) }}</div>
                         <div class="col-md-10">Grupo - Editar</div>
                     </div>
                     <div class="row">
-                        <div class="col-md-10">Grupo - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('grupo.delete', Input::old('grupo.delete', 1)) }}</div>
-                    </div>
+                        <div class="col-md-2">{{ Form::checkBox('grupo_delete', 1) }}</div>
+                        <div class="col-md-10">Grupo - Eliminar</div> 
+                    </div><hr>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('usuario', 1) }}</div>
                         <div class="col-md-10">Usuarios</div>
-                        <div class="col-md-2">{{ Form::checkBox('user', Input::old('user', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('usuario_create', 1) }}</div>
                         <div class="col-md-10">Usuario - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('user.create', Input::old('user.create', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('usuario_update', 1) }}</div>
                         <div class="col-md-10">Usuario - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('user.update', Input::old('user.update', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('usuario_delete', 1) }}</div>
                         <div class="col-md-10">Usuario - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('user.delete', Input::old('user.delete', 1)) }}</div>
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-3 col-sm-2">
+                    </div><hr>
                     <div class="row">
-                        <div class="col-md-10">Biblioteca</div>
-                        <div class="col-md-2">{{ Form::checkBox('libro', Input::old('libro', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Libro - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('libro.create', Input::old('libro.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Libro - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('libro.update', Input::old('libro.update', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Libro - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('libro.delete', Input::old('libro.delete', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Hemeroteca</div>
-                        <div class="col-md-2">{{ Form::checkBox('periodico', Input::old('periodico', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Periodico - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('periodico.create', Input::old('periodico.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Periodico - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('periodico.update', Input::old('periodico.update', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Periodico - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('periodico.delete', Input::old('periodico.delete', 1)) }}</div>
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-3 col-sm-2">
-                    <div class="row">
-                        <div class="col-md-10">Clasificaciones</div>
-                        <div class="col-md-2">{{ Form::checkBox('clasificacion', Input::old('clasificacion', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Clasificación - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('clasificacion.create', Input::old('clasificacion.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Clasificación - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('clasificacion.update', Input::old('clasificacion.update', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Clasificación - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('clasificacion.delete', Input::old('clasificacion.delete', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Estados</div>
-                        <div class="col-md-2">{{ Form::checkBox('estado', Input::old('estado', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Estado - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('estado.create', Input::old('estado.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Estado - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('estado.update', Input::old('estado.update', 1)) }}</div>
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-3 col-sm-2">
-                    <div class="row">
-                        <div class="col-md-10">Estado - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('estado.delete', Input::old('estado.delete', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Tipos</div>
-                        <div class="col-md-2">{{ Form::checkBox('tipo', Input::old('tipo', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Tipo - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('tipo.create', Input::old('tipo.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Tipo - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('tipo.update', Input::old('tipo.update', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Tipo - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('tipo.delete', Input::old('tipo.delete', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Ubicaciones</div>
-                        <div class="col-md-2">{{ Form::checkBox('ubicacion', Input::old('ubicacion', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Ubicación - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('ubicacion.create', Input::old('ubicacion.create', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Ubicación - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('ubicacion.update', Input::old('ubicacion.update', 1)) }}</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10">Ubicación - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('ubicacion.delete', Input::old('ubicacion.delete', 1)) }}</div>
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-3 col-sm-2">
-                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('comentario', 1) }}</div>
                         <div class="col-md-10">Comentarios</div>
-                        <div class="col-md-2">{{ Form::checkBox('comentario', Input::old('comentario', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('comentario_create', 1) }}</div>
                         <div class="col-md-10">Comentario - Crear</div>
-                        <div class="col-md-2">{{ Form::checkBox('comentario.create', Input::old('comentario.create', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('comentario_update', 1) }}</div>
                         <div class="col-md-10">Comentario - Editar</div>
-                        <div class="col-md-2">{{ Form::checkBox('comentario.update', Input::old('comentario.update', 1)) }}</div>
                     </div>
                     <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('comentario_delete', 1) }}</div>
                         <div class="col-md-10">Comentario - Eliminar</div>
-                        <div class="col-md-2">{{ Form::checkBox('comentario.delete', Input::old('comentario.delete', 1)) }}</div>
                     </div>
+                </div>
+                <div class="col-md-3 col-xs-4 col-sm-3">
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('libro', 1) }}</div>
+                        <div class="col-md-10">Biblioteca</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('libro_create', 1) }}</div>
+                        <div class="col-md-10">Libro - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('libro_update', 1) }}</div>
+                        <div class="col-md-10">Libro - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('libro_delete', 1) }}</div>
+                        <div class="col-md-10">Libro - Eliminar</div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('periodico', 1) }}</div>
+                        <div class="col-md-10">Hemeroteca</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('periodico_create', 1) }}</div>
+                        <div class="col-md-10">Periodico - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('periodico_update', 1) }}</div>
+                        <div class="col-md-10">Periodico - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('periodico_delete', 1) }}</div>
+                        <div class="col-md-10">Periodico - Eliminar</div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('clasificacion', 1) }}</div>
+                        <div class="col-md-10">Clasificaciones</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('clasificacion_create', 1) }}</div>
+                        <div class="col-md-10">Clasificación - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('clasificacion_update', 1) }}</div>
+                        <div class="col-md-10">Clasificación - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('clasificacion_delete', 1) }}</div>
+                        <div class="col-md-10">Clasificación - Eliminar</div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-4 col-sm-3">
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('estado', 1) }}</div>
+                        <div class="col-md-10">Estados</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('estado_create', 1) }}</div>
+                        <div class="col-md-10">Estado - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('estado_update', 1) }}</div>
+                        <div class="col-md-10">Estado - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('estado_delete', 1) }}</div>
+                        <div class="col-md-10">Estado - Eliminar</div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('tipo', 1) }}</div>
+                        <div class="col-md-10">Tipos</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('tipo_create', 1) }}</div>
+                        <div class="col-md-10">Tipo - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('tipo_update', 1) }}</div>
+                        <div class="col-md-10">Tipo - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('tipo_delete', 1) }}</div>
+                        <div class="col-md-10">Tipo - Eliminar</div>
+                    </div><hr>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('ubicacion', 1) }}</div>
+                        <div class="col-md-10">Ubicaciones</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('ubicacion_create', 1) }}</div>
+                        <div class="col-md-10">Ubicación - Crear</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('ubicacion_update', 1) }}</div>
+                        <div class="col-md-10">Ubicación - Editar</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-2">{{ Form::checkBox('ubicacion_delete', 1) }}</div>
+                        <div class="col-md-10">Ubicación - Eliminar</div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-xs-3 col-sm-3">
+                    
                 </div>
                 <div class="col-md-2 col-sm-2"></div>
             </div><br>
